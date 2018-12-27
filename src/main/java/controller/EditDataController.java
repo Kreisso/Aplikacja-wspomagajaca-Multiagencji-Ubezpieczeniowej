3813996715fb0c9e9bd96faa8b3eb5a9e136cf9d
Barbara <bszewczyk97@gmail.com>
2018-12-27 20:02:03 +0100

    Merge branch 'master' of C:\Users\Barbara\Documents\Workspace\AWMU with conflicts.

---
+++ src/main/java/controller/EditDataController.java
@@ -0,0 +1,233 @@
+package controller;
+
+import model.Server.Connectivity;
+import model.Server.EditData;
+import view.Frame;
+import view.user.EditDataFrame;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class EditDataController {
+    private EditData model;
+    private EditDataFrame view;
+    private Connectivity con;
+    private Frame previouesView;
+
+    public EditDataController(EditData editData, EditDataFrame editDataFrame, Frame previouesView) {
+        this.model = editData;
+        this.view = editDataFrame;
+        this.previouesView = previouesView;
+        setViewButtonSave();
+    }
+
+    public EditDataController(EditData editData, EditDataFrame editDataFrame, Frame previouesView, Connectivity con) {
+        this.model = editData;
+        this.view = editDataFrame;
+        this.con = con;
+        this.previouesView = previouesView;
+        setViewButtonSave();
+    }
+
+    public String getName() {
+        return model.getName();
+    }
+
+    public void setName(String name) {
+        model.setName(name);
+    }
+
+    public String getSurname() {
+        return model.getSurname();
+    }
+
+    public void setSurname(String surname) {
+        model.setSurname(surname);
+    }
+
+    public String getCity() {
+        return model.getCity();
+    }
+
+    public void setCity(String city) {
+        model.setCity(city);
+    }
+
+    public String getStreetAndNumber() {
+        return model.getStreetAndNumber();
+    }
+
+    public void setStreetAndNumber(String streetAndNumber) {
+        model.setStreetAndNumber(streetAndNumber);
+    }
+
+    public String getPostCode() {
+        return model.getPostCode();
+    }
+
+    public void setPostCode(String postCode) {
+        model.setPostCode(postCode);
+    }
+
+    public String getPhoneNumber() {
+        return model.getPhone();
+    }
+
+    public void setPhoneNumber(String phone) {
+        model.setPhone(phone);
+    }
+
+    private String getErrorMessage()
+    {
+        return model.getErrorMessage();
+    }
+
+    private void setErrorMessage(String errorMessage)
+    {
+        model.setErrorMessage(errorMessage);
+    }
+
+    private void setViewButtonSave()
+    {
+        view.setSaveButton(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                System.out.println("click register button");
+                saveData();
+            }
+        });
+    }
+
+    private void saveData() {
+
+        PreparedStatement preparedStatement = null;
+        ResultSet resultSet = null;
+        setErrorMessage("");
+        String err = "";
+
+        //name
+        String name = String.valueOf(view.getInputName());
+        Pattern pattern = Pattern.compile("[a-zA-Z]+");
+        Matcher matcher = pattern.matcher(name);
+        if (!matcher.matches()) {
+            err += "Nie wpisano imienia lub wpisano niepoprawne - imię może składać się tylko z liter alfabetu\n";
+        }
+        //surname
+        String surname = String.valueOf(view.getInputSurname());
+        pattern = Pattern.compile("[a-zA-Z]+");
+        matcher = pattern.matcher(surname);
+        if (!matcher.matches()) {
+            err += "Nie wpisano nazwiska lub wpisano niepoprawne - nazwisko może składać się tylko z liter alfabetu\n";
+        }
+        //City
+        String city = String.valueOf(view.getInputCity());
+        pattern = Pattern.compile("[a-zA-Z]+");
+        matcher = pattern.matcher(city);
+        if (!matcher.matches()) {
+            err += "Nie wpisano miasta lub wpisano niepoprawne - miasto może składać się tylko z liter alfabetu\n";
+        }
+        //street
+        String street = String.valueOf(view.getInputStreetAndNo());
+        pattern = Pattern.compile("[a-zA-Z]+ [a-zA-Z0-9/]+");
+        matcher = pattern.matcher(street);
+        if (!matcher.matches()) {
+            err += "Nie wpisano ulicy i numeru lub wpisano niepoprawne - miasto może składać się tylko z liter alfabetu, a po spacji musi znaleźć się numer budynku\n";
+        }
+        //postcode
+        String postCode = String.valueOf(view.getInputPostCode());
+        pattern = Pattern.compile("\\d{2}-\\d{3}");
+        matcher = pattern.matcher(postCode);
+        if (!matcher.matches()) {
+            err += "Nie wpisano kodu pocztowego lub wpisano niepoprawny - kod pocztowy musi miec format xx-xxx\n";
+        }
+        //phoneNumber
+        String phoneNumber = String.valueOf(view.getInputPhoneNo());
+        pattern = Pattern.compile("\\d{3}[ -]*\\d{3}[ -]*\\d{3}|\\d{2}[ -]*\\d{3}[ -]*\\d{2}[ -]*\\d{2}"); //komorkowy | stacjonarny
+        matcher = pattern.matcher(phoneNumber);
+        if (!matcher.matches()) {
+            err += "Nie wpisano numeru telefonu lub wpisano niepoprawny - telefon stacjonarny musi zawierać numer kierunkowy\n";
+        }
+
+/**
+ *  1 Login
+ *  2 Pass
+ *  3 Pesel
+ *  4 Name
+ *  5 Surname
+ *  6 City
+ *  7 Streat and number
+ *  8 PostCode
+ *  9 phoneNumber
+ *
+ *
+ */
+        if (err == "") {
+            String sql = "{call addClient(?,?,?,?,?,?,?,?,?)}";
+            try {
+                con = new Connectivity();
+
+                if (isAdded(view.getInputLogin(), con)) {
+                    err += "Podany login już istnieje \n";
+
+
+                    //todo
+
+                } else if (err.length() < 1) {
+                    setName(view.getInputName());
+                    setSurname(view.getInputSurname());
+                    setCity(view.getInputCity());
+                    setStreetAndNumber(view.getInputStreetAndNo());
+                    setPostCode(view.getInputPostCode());
+                    setPhoneNumber(view.getInputPhoneNo());
+
+                    preparedStatement = con.getConn().prepareStatement(sql);
+
+                    preparedStatement.setString(4, getName());
+                    preparedStatement.setString(5, getSurname());
+                    preparedStatement.setString(6, getCity());
+                    preparedStatement.setString(7, getStreetAndNumber());
+                    preparedStatement.setString(8, getPostCode());
+                    preparedStatement.setString(9, getPhoneNumber());
+
+                    resultSet = preparedStatement.executeQuery();
+                    if (isAdded(getLogin(), con)) {
+                        view.dispose();
+                        previouesView.setVisible(true);
+
+                    } else {
+
+                        view.setErrorLabel(err);
+                    }
+                }
+
+                if (err.length() > 1) {
+                    setErrorMessage(err);
+                    System.out.println(getErrorMessage());
+
+
+                }
+
+
+            } catch (SQLException ex) {
+                System.out.println(ex);
+                con.close();
+            } catch (Exception e) {
+                System.out.println(e);
+                con.close();
+            } finally {
+                con.close();
+            }
+
+        }
+        else
+        {
+            view.setErrorLabel(err);
+            System.out.println("Wiadomość o błędach \n"+err);
+        }
+    }
+
+}
---
+++ src/main/java/model/Server/EditData.java
@@ -0,0 +1,67 @@
+package model.Server;
+
+public class EditData {
+    private String name;
+    private String surname;
+    private String city;
+    private String streatAndNumber;
+    private String postCode;
+    private String phone;
+    private String errorMessage;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public void setSurname(String surname) {
+        this.surname = surname;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getStreetAndNumber() {
+        return streatAndNumber;
+    }
+
+    public void setStreetAndNumber(String streatAndNumber) {
+        this.streatAndNumber = streatAndNumber;
+    }
+
+    public String getPostCode() {
+        return postCode;
+    }
+
+    public void setPostCode(String postCode) {
+        this.postCode = postCode;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public String getErrorMessage() {
+        return errorMessage;
+    }
+
+    public void setErrorMessage(String errorMessage) {
+        this.errorMessage = errorMessage;
+    }
+}
