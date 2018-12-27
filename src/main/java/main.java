2cef423c46a18114765c226365d72da505e5ba8e
Maciej Polak <polakmaciej.web@gmail.com>
2018-12-27 13:11:07 +0100

    Refactoring

--- src/main/java/main.java
+++ src/main/java/main.java
@@ -1,3 +1,5 @@
+import controller.LoginController;
+import model.Server.Login;
 import view.loginpanel.LoginFrame;
 import java.awt.EventQueue;
 
@@ -6,10 +8,9 @@ public class main {
     public static void main(String[] args){
         EventQueue.invokeLater(new Runnable() {
             public void run() {
-                new LoginFrame("Logowanie");
+                new LoginController(new Login(), new LoginFrame("Logowanie"));
             }
         });
 
     }
-
 }
