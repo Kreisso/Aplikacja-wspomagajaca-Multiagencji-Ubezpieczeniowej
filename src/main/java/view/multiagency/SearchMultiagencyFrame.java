c16b7316e0c1dfd75d576e1088cfcd1b79d50bbb
Barbara <bszewczyk97@gmail.com>
2018-12-27 13:18:23 +0100

    Merge branch 'Barbara' of C:\Users\Barbara\Documents\Workspace\Aplikacja-wspomagajaca-Multiagencji-Ubezpieczeniowej with conflicts.

--- src/main/java/view/multiagency/SearchMultiagencyFrame.java
+++ src/main/java/view/multiagency/SearchMultiagencyFrame.java
@@ -10,5 +10,52 @@ import java.awt.*;
 public class SearchMultiagencyFrame extends view.Frame {
     public SearchMultiagencyFrame(String name) throws HeadlessException {
         super(name);
+<<<<<<< Updated upstream
+=======
+
+        int screenWidth = Toolkit.getDefaultToolkit().getScreenSize().width;
+        int screenHeight = Toolkit.getDefaultToolkit().getScreenSize().height;
+        this.setLocation((screenWidth-frameWidth)/2, (screenHeight-frameHeight)/2);
+
+        this.setSize(frameWidth, frameHeight);
+        this.setLayout(null);
+        
+        this.createClientMenu();
+
+        searchByCityLabel = new JLabel("Wpisz swoje miasto");
+        searchByCityLabel.setSize(searchByCityLabel.getPreferredSize());
+        searchByCityLabel.setLocation((int) (frameWidth * 0.05), (int) (frameHeight * 0.05)-20);
+        this.add(searchByCityLabel);
+
+        inputSearchByCity = new JTextField();
+        inputSearchByCity.setSize((int) (frameWidth * 0.7), 40);
+        inputSearchByCity.setLocation((int) (frameWidth * 0.05), (int) (frameHeight * 0.05));
+        this.add(inputSearchByCity);
+
+        searchButton = new JButton("Szukaj");
+        searchButton.setSize((int) (frameWidth*0.15), 40);
+        searchButton.setLocation((int) (frameWidth*0.8),  (int) (frameHeight*0.05));
+        this.getRootPane().setDefaultButton(searchButton);
+        this.add(searchButton);
+
+        multiagencyTable = new JTable();
+        model = new DefaultTableModel();
+        model.addColumn("LP");
+        model.addColumn("Nazwa agencji");
+        model.addColumn("Miasto");
+        model.addColumn("Ulica i numer");
+        multiagencyTable = new JTable(model);
+        scrollPane = new JScrollPane(multiagencyTable);
+        scrollPane.setSize((int) (frameWidth * 0.7), (int) (frameHeight * 0.7));
+        scrollPane.setLocation((int) (frameWidth * 0.05), (int) (frameHeight * 0.15));
+        this.add(scrollPane);
+
+
+//        MultiagenciesPanel multiagenciesPanel = new MultiagenciesPanel();
+//
+//            multiagenciesPanel.setVisible(true);
+//
+//        this.add(multiagenciesPanel);
+>>>>>>> Stashed changes
     }
 }
